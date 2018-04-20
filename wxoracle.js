63bb9b578f33de952076bf8268f4fa8cccbd0f0f
= <=>
2018-04-19 21:02:47 -0700

    talks to EVM.  runs contracts

--- wxoracle.js
+++ wxoracle.js
@@ -4,74 +4,112 @@ Weather Conditions Oracle
 use as you please at your own risk
  */
 
-
-var Web3 = require('web3')
+let Web3 = require('web3')
+let fs = require('fs')
+let request = require('request')
 
 // using a Websockets URL.  Ganache works with Web3 1.0 and Websockets
-const testNetWS = "ws://127.0.0.1:7545"
+const testNetWS = "ws://127.0.0.1:8545"
 
 // create a WebsocketProvider
-console.log("starting network")
+console.log("connecting to network")
 const web3 = new Web3(new Web3.providers.WebsocketProvider(testNetWS))
 
-// account address - we're using Ganache here, so this may look familiar
-const address = "0x627306090abaB3A6e1400e9345bC60c78a8BEf57"
-var contractAddress = null
+// account address
+const account = "0x3f1e08ea03e6d4e9151c5b5389565d81e696f673"
 
 // we're using the Dark Sky API - get your API key at darksky.net
 const apiKey = "70ad7f20bbf194a50e3375d087bb8969"
 
+// store the ABI for the contract so we can use it later
+let abi = null
+
+
 // if a contract address is passed in on the command line, use it
-// //otherwise: load, compile and deploy the included contract (contract.sol)
-if (!process.argv[2]) {
-    contractAddress = loadCompileDeploy("contract.sol")
-} else {
-    contractAddress = process.argv[2]
-    console.log("contract address passed in: " + contractAddress)
+// otherwise: load, compile and deploy the included contract (contract.sol)
+let c = loadCompileDeploy("contract.sol", ":WeatherOracle")
+c.then(function(address) {
+    console.log("contract address: " + address)
+    handler(address)
+    start(address)
+}, function(err) {
+    console.log("shit didn't work.  here's why: " + err)
+})
+
+
+// starts the event listener
+function start(address) {
+    console.log("starting event monitoring on contract: " + address)
+    web3.eth.subscribe('logs', {address: address}, function (error, result) {
+        if (error)
+            console.log("error received: " + error);
+        else {
+            console.log('event received' + result);
+            handler(address)
+        }
+    });
 }
 
-console.log("starting event monitoring")
-web3.eth.subscribe('logs', { address: contractAddress}, function(error, result) {
-    if(error)
-        console.log("error received: " + error);
-    else
-        console.log('event received' + result);
-});
+// handles a request event and sends the response to the contract
+function handler(address) {
+    // using the excellent Dark Sky API - getting the forecast, centered on LA
+    let url = "https://api.darksky.net/forecast/" + apiKey + "/37.8267,-122.4233"
+    request(url, function(error, response, body) {
+        if(error)
+            console.log("error: " + error)
+        console.log("status code: " + response.statusCode)
+        let wx = JSON.parse(body)
+        let temperature = Math.round(wx.currently.temperature)
+        console.log("temp: " + temperature)
+
+        // run the 'fill' method on the contract
+        var MyContract = new web3.eth.Contract(abi, address);
+        MyContract.methods.fill(temperature).call()
+            .then(function(result) {
+                console.log("EVM call result: " + result)
+                if (temperature == result)
+                    console.log("+++success+++")
+            }, function(error) {
+                console.log("error "  + error)
+            })
+
+    })
+}
 
 
 // Load, Compile and Deploy the contract
 // returns a contract address
-function loadCompileDeploy(filename) {
+async function loadCompileDeploy(filename, contractName) {
     const solc = require('solc')
-    var code = fs.readFileSync(filename).toString('utf-8')
+    let code = fs.readFileSync(filename).toString('utf-8')
 
     // compile single contract
     console.log('compiling contract ' + filename)
-    var output = solc.compile(code, 1)
-    for (var contractName in output.contracts) {
+    let output = solc.compile(code, 1)
+    let bc = output.contracts[contractName].bytecode
+    abi = JSON.parse(output.contracts[contractName].interface)
+    let contract = new web3.eth.Contract(abi)
+    let addr = ""
 
-        // deploy a single contract
-        console.log('deploying contract ' + contractName)
-        var bc = output.contracts[contractName].bytecode
-        var abi = JSON.parse(output.contracts[contractName].interface)
-        var contract = new web3.eth.Contract(abi)
+    // deploy the contract
+    let x = contract.deploy({
+        data: '0x' + bc
+    }).send({
+        from: account,
+        gas: 555555
+    }, function(error, transactionHash) {
+        if(error) {
+            console.log("error: " + error)
+        }
+        if(transactionHash)
+            console.log("txId: " + transactionHash)
+    }).on('receipt', function(receipt) {
+        addr = receipt.contractAddress
+    }).then(function(contractInstance){
 
-        contract.deploy({
-            data: '0x' + bc
-        }).send({
-            from: address,
-            gas: 555555
-        }, function(error, transactionHash) {
-            if(error)
-                console.log("error: " + error)
-            if(transactionHash)
-                console.log("txId: " + transactionHash)
-        }).on('receipt', function(receipt) {
-            contractAddress = receipt.contractAddress
-            console.log("contract address: " + contractAddress)
-        })
-    }
-    return contractAddress
+    })
+    await x
+    return addr
 
 }
 
